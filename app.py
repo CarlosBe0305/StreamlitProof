a5dc05530387a1c2e3d6c80eecae5f5244df9e30
Carlos Alejandro Benítez Hoyos <cabenitez0305@gmail.com>
2023-08-24 07:09:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from PIL import Imaage
 
 st.title('Mi primera Aplicación')
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para Interfaces Multimodales")
 st.write('Carlos Benitez')
+image = Image.open('broccoli-8174625.jpg')

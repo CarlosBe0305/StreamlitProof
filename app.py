e3ffb5af45ef022e5824914f1a1b63612b7cc38f
Carlos Alejandro Benítez Hoyos <cabenitez0305@gmail.com>
2023-08-24 07:12:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ st.title('Mi primera Aplicación')
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para Interfaces Multimodales")
 st.write('Carlos Benitez')
 image = Image.open('broccoli-8174625.jpg')
+
+st.image(image, caption= 'Interfaces Multimodales')
